437bc4db43f29e719ca4c4d0264952f9b8cc82c6
Russell Smith <Russll@eduroam-160-74.dyn.carleton.edu>
2017-03-11 22:49:35 -0600

    minions now spawn from building locations

--- source/Controller/FightController.java
+++ source/Controller/FightController.java
@@ -1,12 +1,13 @@
 package Controller;
 
 
-import Model.CombatManager;
-import Model.MinionImpl;
-import Model.Player;
-import Model.PlayerImpl;
+import Model.*;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.animation.AnimationTimer;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
 
 public class FightController implements FightControllerInterface {
     private PlayerImpl[] players;
@@ -20,23 +21,49 @@ public class FightController implements FightControllerInterface {
         this.players = players;
     }
 
-    public void runFight(CombatManager manager, GraphicsContext graphics) {
+    public void runFight(CombatManager manager, GraphicsContext graphics, GraphicsContext graphics1) {
         PlayerImpl player1 = players[0];
         PlayerImpl player2 = players[1];
 
         player1.setOpponent(player2);
 
+        System.out.println(player1.getBuildings().size());
+        System.out.println(player2.getBuildings().size());
+
+        for (int j = 0; j < player1.getBuildings().size(); j = j + 1) {
+            BuildingImpl building = player1.getBuildings().get(j);
+            building.spawnMinion(manager);
+        }
+        for (int k = 0; k < player2.getBuildings().size(); k = k + 1) {
+            BuildingImpl building = player2.getBuildings().get(k);
+            building.spawnMinion(manager);
+        }
+
+
         new AnimationTimer() {
             @Override
             public void handle(long now) {
                 try {
-                    Thread.sleep(100);
+                    Thread.sleep(50);
                 }
                 catch (InterruptedException e) {
                     System.out.println(e);
                 }
 
                 manager.doCombat(player1, player2);
+
+                /*
+                try {
+                    AudioInputStream audioIn = AudioSystem.getAudioInputStream(MinionImpl.class.getResource("MinionGrunt.wav"));
+                    Clip clip = AudioSystem.getClip();
+                    clip.open(audioIn);
+                    clip.start();
+                    }
+                catch (Exception e) {
+                    System.out.println(e);
+                    }
+                    */
+
                 graphics.clearRect(0,0,2000,1000);
                 for (int i = 0; i < manager.getAllInstances().size(); i = i + 1) {
                     MinionImpl minion = manager.getAllInstances().get(i);
@@ -53,5 +80,19 @@ public class FightController implements FightControllerInterface {
 
             }
         }.start();
+    }
+
+    public void showBuildings(GraphicsContext gc) {
+        PlayerImpl player1 = players[0];
+        PlayerImpl player2 = players[1];
+
+        for (int j = 0; j < player1.getBuildings().size(); j = j + 1) {
+            BuildingImpl building = player1.getBuildings().get(j);
+            building.render(gc);
+        }
+        for (int k = 0; k < player2.getBuildings().size(); k = k + 1) {
+            BuildingImpl building = player2.getBuildings().get(k);
+            building.render(gc);
+        }
     }
 }
