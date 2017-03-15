e8d60a0af2f2401ca114d28d74530762967851c5
xxf1995 <xiax@carleton.edu>
2017-03-15 16:41:01 -0500

    fix end game

--- source/Model/CombatManager.java
+++ source/Model/CombatManager.java
@@ -80,11 +80,11 @@ public class CombatManager {
                 //if the minion has no king then it hasn't teleported yet so check portal
                 instances.get(i).checkPortal();
                 instances.get(i).performAttack(instances.get(i).master.opponent.getMinions());
+            } else {
+                //otherwise attack
                 if (instances.get(i).myKing.checkDeath() || instances.get(i).myKing.opponetKing.checkDeath()) {
                     break;
                 }
-            } else {
-                //otherwise attack
                 instances.get(i).performAttack(instances.get(i).master.myKing.getOpponetKing().getMinions());
             }
         }
